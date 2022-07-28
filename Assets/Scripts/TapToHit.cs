da0e89f5d5a432f002de1db4a6d2c42301c876b6
alhajjajiwid <110183350+alhajjajiwid@users.noreply.github.com>
2022-07-28 23:35:51 +0300

    Sound Design finished

--- Assets/Scripts/TapToHit.cs
+++ Assets/Scripts/TapToHit.cs
@@ -7,9 +7,12 @@ public class TapToHit : MonoBehaviour
 {
 
     public GameObject destuctionParticles;
+    public AudioSource destroySfx;
+    public AudioSource hitSfx;
         
     private void OnMouseDown()
     {
+        destroySfx.Play();
         Destroy(gameObject);
     }
 
@@ -21,6 +24,7 @@ public class TapToHit : MonoBehaviour
             if (dolly.IsBoosted == false)
             {
                 dolly.SlowDownCar();
+                hitSfx.Play();
             }
             Instantiate(destuctionParticles, transform.position, transform.rotation);
             Destroy(gameObject);
